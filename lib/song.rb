624c09bdfa3aa593e811d05d9bee50e6151db054
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-17 01:01:41 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song
+    
+    attr_accessor :name, :artist
+    
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        self.save
+    end
+    
+    # def artist=(artist)
+    #     @artist = artist
+    #     Artist.count << self
+    # end  
+    
+    def artist
+        @artist
+    end   
+    
+    def artist_name
+      if self.artist
+          self.artist.name
+      else  
+          nil
+      end      
+    end   
+    
+    def self.all
+        @@all
+    end
+    
+    def save
+        @@all << self
+    end
+    
+    
+    
+    
+end 
