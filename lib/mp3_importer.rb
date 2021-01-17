ac94742e5907542117d3dfcca51e0805a786415c
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-17 19:00:55 +0000

    commit tree af4631dc321fc565bbcec8de1e98cdf130b72afc

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -3,17 +3,17 @@ class MP3Importer
  attr_reader :path
     
  def initialize(path)
-  path = path
+  @path = path
      
  end 
  
- # def files
- #  @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
- # end 
+ def files
+  @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+ end 
  
- # def import
- #     files.each{|f| Song.new_by_filename(f)}
- # end
+ def import
+     files.each{|f| Song.new_by_filename(f)}
+ end
 end 
 
 # require 'spec_helper'
